b3075cb0c8cb283a7f30b044862ab94982f4f6d9
marklovejoydev <marklovejoydev@gmail.com>
2023-08-04 16:29:39 +0100

    new push notification

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:whos_doing_the_dishes/pages/authpage.dart';
+import 'package:whos_doing_the_dishes/services/push_notifications.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await FirebaseMessaging.instance.getInitialMessage();
+  await pushNotifications().initNotifications();
   runApp(const MyApp());
 }
 
