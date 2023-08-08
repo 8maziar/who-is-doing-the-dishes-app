4b95e44a0ca9ed8a293a9522473c26f8dd667056
Hersh Thakur <hersh.thakur@gmail.com>
2023-08-08 08:57:18 +0100

    styling the profile page

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         home: const AuthPage(),
         theme: ThemeData(
-          scaffoldBackgroundColor: Color.fromARGB(255, 216, 232, 245),
+          scaffoldBackgroundColor: const Color.fromARGB(255, 216, 232, 245),
         ));
   }
 }
