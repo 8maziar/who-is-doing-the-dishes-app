f1edf7b943584838a832e925fa556600a648c787
Alex Croden <alexcroden@hotmail.com>
2023-08-09 15:28:51 +0100

    final push

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -1,90 +1,53 @@
-
-        import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
         import 'package:firebase_auth/firebase_auth.dart';
         import '../components/task_tile.dart';
         import "package:google_fonts/google_fonts.dart";
         import 'package:lottie/lottie.dart';
         import 'select_avatar.dart';
         import 'package:cloud_firestore/cloud_firestore.dart';
-
-class AccountPage extends StatefulWidget {
-  const AccountPage({super.key});
-
-  @override
-  _AccountPageState createState() => _AccountPageState();
-}
-
-
+          class AccountPage extends StatefulWidget {
+            const AccountPage({super.key});
+            @override
+            _AccountPageState createState() => _AccountPageState();
+          }
           class _AccountPageState extends State<AccountPage> {
             bool _showChangeButton = false;
              String? _avatarUrl;
-
-
-  @override
-  void initState() {
-    super.initState();
-
+            @override
+            void initState() {
+              super.initState();
              getUserAvatarUrl().then((avatarUrl) {
                 setState(() {
                   _avatarUrl = avatarUrl;
                 });
               });
-
               Future.delayed(Duration(seconds: 1), () {
                 setState(() {
                   _showChangeButton = true;
                 });
               });
             }
-
-
           Future<String?> getUserAvatarUrl() async {
             final User? user = FirebaseAuth.instance.currentUser;
             if (user != null) {
               DocumentSnapshot<Map<String, dynamic>> userDoc =
                   await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
-
               if (userDoc.exists) {
                 return userDoc.get('avatar') ?? null;
               }
             }
             return null;
           }
-
-
             @override
             Widget build(BuildContext context) {
               final User? user = FirebaseAuth.instance.currentUser;
-
-
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 219, 228, 236),
-      appBar: AppBar(
-        title: Text(
-          'Account',
-          style: GoogleFonts.roboto(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Stack(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(top: 30.0),
-                    child: _selectedImage != null
-                        ? Image.file(
-                            _selectedImage!,
-                            height: 250,
-                          )
-                        : Lottie.network(
-                            'https://lottie.host/c772a776-15f3-4289-95b9-42267e43b322/Gg7NDzFyfs.json',
-                            height: 250,
-                          ),
+              return Scaffold(
+                backgroundColor: const Color.fromARGB(255, 219, 228, 236),
+                appBar: AppBar(
+                  title: Text(
+                    'Account',
+                    style: GoogleFonts.roboto(fontSize: 24, fontWeight: FontWeight.bold),
                   ),
-
                 ),
                 body: SingleChildScrollView(
                   child: Padding(
@@ -96,7 +59,6 @@ class AccountPage extends StatefulWidget {
                             Container(
                               margin: EdgeInsets.only(top: 30.0),
                               child: _avatarUrl != null
-                              
                                         ? Lottie.network(
                                             _avatarUrl!,
                                             height: 250,
@@ -106,7 +68,6 @@ class AccountPage extends StatefulWidget {
                                                   height: 250,
                                                 )
                             ),
-                            
                             Padding(
                               padding: const EdgeInsets.only(top: 15.0),
                               child: Text(
@@ -134,7 +95,6 @@ class AccountPage extends StatefulWidget {
                                                 builder: (context) => AvatarSelectionPage(),
                                               ),
                                             );
-
                                             if (selectedAvatarUrl != null) {
                                               setState(() {
                                                 _avatarUrl = selectedAvatarUrl;
@@ -152,15 +112,7 @@ class AccountPage extends StatefulWidget {
                                 ),
                               ),
                           ],
-
                         ),
-                      ),
-                    ),
-                ],
-              ),
-
-
-
                         SizedBox(height: 20),
                         const TaskTile(userTask: "Do The Dishes"),
                         SizedBox(height: 10),
@@ -169,7 +121,6 @@ class AccountPage extends StatefulWidget {
                         const TaskTile(userTask: "Do The Groceries"),
                         SizedBox(height: 10),
                         const TaskTile(userTask: "Take The Kids To School"),
-
                       ],
                     ),
                   ),
@@ -177,53 +128,3 @@ class AccountPage extends StatefulWidget {
               );
             }
           }
-
-              /*  
-                    FRIENDS: 
-                      Positioned(
-                          top: MediaQuery.of(context).size.height / 2 - 30, // Vertically centered
-                          right: 16.0, // Aligned to the right side
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                                          Text(
-                            "Friends:",
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                              SizedBox(height: 5, width: 5,),
-                              FloatingActionButton(
-                                onPressed: () {
-
-                                },
-                                child: Lottie.network('https://lottie.host/574176dd-97a5-4a71-8801-c2805e95706d/rZxxNC6sQK.json'),
-                              ),
-                              SizedBox(height: 5, width: 5,),
-                              FloatingActionButton(
-                                onPressed: () {
-
-                                },
-                                child: Lottie.network('https://lottie.host/49128aea-acca-4a50-a6bd-a5e4e98f9925/zAdrGKPK8w.json'),
-                              ),
-
-                            ],
-                          ),
-                        ), */
-              const SizedBox(height: 20),
-              const TaskTile(userTask: "Do The Dishes"),
-              const SizedBox(height: 10),
-              const TaskTile(userTask: "Wash The Dog"),
-              const SizedBox(height: 10),
-              const TaskTile(userTask: "Do The Groceries"),
-              const SizedBox(height: 10),
-              const TaskTile(userTask: "Take The Kids To School"),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
